741bb6022e3a7700fca1462e2ec8590599f9c376
tejuingalagi <tejuingalagi07@gmail.com>
2026-04-02 21:55:31 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/finance/FinanceDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDashboardBackendApplication.class, args);
+	}
+
+}
